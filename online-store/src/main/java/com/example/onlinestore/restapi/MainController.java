0bdce4d1a397ecbf793a36e97537a508fd39a11a
unknown <nhutnguyen5691@gmail.com>
2021-11-17 20:47:48 -0800

    restapi/order functions in progress

--- online-store/src/main/java/com/example/onlinestore/restapi/MainController.java
+++ online-store/src/main/java/com/example/onlinestore/restapi/MainController.java
@@ -2,6 +2,7 @@ package com.example.onlinestore.restapi;
 
 import java.util.Random;
 import java.util.List;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,14 +11,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+
 
 @RestController
 public class MainController {
+    private TicketRepository ticketRepository;// our main object to manage
+    @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
     private CardRepository cardRepository;
-    private TicketRepository ticketRepository;
+    private HashMap<String, Ticket> orders = new HashMap<>();
 
     MainController(CustomerRepository customerRepository, CardRepository cardRepository, TicketRepository ticketRepository) {
         this.customerRepository = customerRepository;
@@ -51,7 +59,7 @@ public class MainController {
 
     //Get all the cards in database
     @GetMapping("/cards")
-    List<Card> all() {
+    List<Card> allCards() {
         return cardRepository.findAll();
     }
 
@@ -82,5 +90,123 @@ public class MainController {
         return card;
     }
 
+    //Submit an order
+    @PostMapping("/order/register/{regid}")
+    @ResponseStatus(HttpStatus.CREATED)
+    Ticket newOrder(@PathVariable String regid, @RequestBody Ticket order) {
+        System.out.println("Placing Order (Reg ID =" + regid + ") => " + order);
+        if (order.getDrink().equals("") || order.getMilk().equals("") ||
+        order.getDrinksize().equals("")) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Order Request!");
+        }
+
+        Ticket active = orders.get(regid);
+        if (active != null) {
+            System.out.println("Active Order (Reg ID = " + regid + ") +> " + active);
+            if (active.getStatus().equals("Ready for Payment.")) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Active Order Exists!");
+            }
+                
+        }
+        double price = 0.0;
+        switch (order.getDrink()) {
+        case "Jasmine Milktea":
+            switch (order.getDrinksize()) {
+            case "Small":
+                price = 2.95;
+                break;
+            case "Regular":
+                price = 3.65;
+                break;
+            case "Large":
+            case "Your Own Cup":
+                price = 3.95;
+                break;
+            default:
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Size!");
+            }
+            break;
+        case "Matcha Greentea":
+            switch (order.getDrinksize()) {
+            case "Small":
+                price = 2.25;
+                break;
+            case "Regular":
+                price = 2.65;
+                break;
+            case "Large":
+            case "Your Own Cup":
+                price = 2.95;
+                break;
+            default:
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Size!");
+            }
+            break;
+        case "Milk Coffe":
+            switch (order.getDrinksize())  {
+            case "Small":
+                price = 3.45;
+                break;
+            case "Regular":
+                price = 4.15;
+                break;
+            case "Large":
+            case "Your Own Cup":
+                price = 4.45;
+                break;
+            default:
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Size!");
+            }
+            break;
+        case "Lychee Slush":
+            switch (order.getDrinksize()) {
+            case "Small":
+                price = 4.25;
+                break;
+            case "Regular":
+                price = 4.65;
+                break;
+            case "Large":
+            case "Your Own Cup":
+                price = 4.95;
+                break;
+            default:
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Size!");
+            }
+            break;
+        default:
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Drink!");
+        }
+
+        double tax = 0.0456;
+        double total = price * (price + tax);
+        double scale = Math.pow(10,2);
+        double rounded = Math.round(total + scale) / scale;
+        order.setTotal(rounded);
+        order.setStatus("Ready for Payment.");
+        Ticket new_order = ticketRepository.save(order);
+        ticketRepository.put(regid, new_order);
+        return new_order;
+             
+    }
+
+    //Get list of all orders
+    @GetMapping("/orders")
+    List<Ticket> allOrders() {
+        return ticketRepository.findAll();
+    }
+
+    //Get specific order with ID
+    @GetMapping("/order/register/{regid}")
+    Ticket getActiveOrder(@PathVariable String regid, HttpServletResponse response) {
+        Ticket active = orders.get(regid);
+        if (active != null) {
+            return active;
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Order Not Found!");
+        }
+    }
+
     //
+
 }
