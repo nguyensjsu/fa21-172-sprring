f33dcdf9c0d7a411272e8fe839fc64d0e0db1eb4
Rozanna Abramtsis <zanagen@yahoo.com>
2021-11-24 16:44:34 -0800

    added spring payments folder from lab 7, added CyberSource API key

--- payment/src/main/java/com/example/payment/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.payment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface CardRepository extends JpaRepository<Card, Integer> {
-    Card findByCardnumber(String cardnumber);
-    void deleteByCardnumber(String cardnumber);
-}
---
+++ payment/src/main/java/com/example/springpayments/PaymentsCommand.java
@@ -0,0 +1,51 @@
+package com.example.springpayments;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+
+@Entity
+@Table()
+@Data
+@RequiredArgsConstructor
+class PaymentsCommand {
+
+    private @Id @GeneratedValue Long id;
+    private String action;
+    private String firstname;
+    private String lastname;
+
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phonenumber;
+    private String cardnumber;
+    private String expmonth;
+    private String expyear;
+    private String cvv;
+    private String email;
+    private String notes;
+
+    // accept only 4 types of cards
+    // visa, amex, mastercard, and discover
+    private String cardtype;
+
+    // for executing payment through CyberSource
+    private String ordernumber;
+    private String transactionamount;
+    private String transactioncurrency;
+    private String authid;
+    private String authstatus;
+    private String captureid;
+    private String capturestatus;
+}
