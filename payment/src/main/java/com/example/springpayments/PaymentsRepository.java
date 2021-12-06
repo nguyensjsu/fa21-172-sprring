2e2c071e2a60c116a618e989394e1d531f0a4fe2
Rozanna Abramtsis <zanagen@yahoo.com>
2021-12-05 16:12:25 -0800

    fixed compiler error about repository types

--- payment/src/main/java/com/example/springpayments/PaymentsRepository.java
+++ payment/src/main/java/com/example/springpayments/PaymentsRepository.java
@@ -1,10 +1,11 @@
 package com.example.springpayments;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// This will be AUTO IMPLEMENTED by Spring into a Bean called PaymentsRepository
+// inherits CRUD repository
 // CRUD refers Create, Read, Update, Delete
 
-public interface PaymentsRepository extends CrudRepository<PaymentsCommand, Long> {
+public interface PaymentsRepository extends JpaRepository<PaymentsCommand, Long> {
 
 }
