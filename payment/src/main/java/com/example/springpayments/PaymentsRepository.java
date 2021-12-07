ea17f56b2c74c0ed1c750273d8a7526761b029a1
Rozanna Abramtsis <zanagen@yahoo.com>
2021-12-06 17:28:36 -0800

    added payments

--- payment/src/main/java/com/example/springpayments/PaymentsRepository.java
+++ payment/src/main/java/com/example/springpayments/PaymentsRepository.java
@@ -1,11 +1,10 @@
 package com.example.springpayments;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called PaymentsRepository
-// inherits CRUD repository
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface PaymentsRepository extends JpaRepository<PaymentsCommand, Long> {
+public interface PaymentsRepository extends CrudRepository<PaymentsCommand, Integer> {
 
 }
